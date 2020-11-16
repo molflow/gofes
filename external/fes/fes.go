18cb7ce2c27bccbc63765edd57061899fd7adf7e
Martin Zackrisson <martin.zackrisson@molflow.com>
2020-11-16 14:54:32 +0100

    Fixes

--- external/fes/fes.go
+++ external/fes/fes.go
@@ -1,5 +1,6 @@
 package fes
 
+// #cgo LDFLAGS: -lfes
 // #include <stdlib.h>
 // #include <fes.h>
 import "C"
@@ -40,15 +41,17 @@ type Tide struct {
 
 // Fes ...
 type Fes struct {
-	path   *C.char
 	handle *C.FES
 }
 
 // Tide ...
 func (fes *Fes) Tide(lat float64, lon float64, time time.Time) (Tide, error) {
+	if fes.handle == nil {
+		return Tide{}, fmt.Errorf("")
+	}
 	h := C.double(math.NaN())
 	hLongPeriod := C.double(math.NaN())
-	julian := cnes.CNESJulian(time)
+	julian := cnes.JulianDate(time)
 
 	ok := C.fes_core(
 		*fes.handle,
@@ -70,19 +73,18 @@ func (fes *Fes) Tide(lat float64, lon float64, time time.Time) (Tide, error) {
 func (fes *Fes) Close() {
 	C.fes_delete(*fes.handle)
 	C.free(unsafe.Pointer(fes.handle))
-	C.free(unsafe.Pointer(fes.path))
 	fes.handle = nil
-	fes.path = nil
 }
 
 // NewFes creates a new Fes interface
 func NewFes(tideType TideType, mode Mode, path string) (*Fes, error) {
-	var handle *C.FES
+	var handle C.FES
 	var cpath *C.char = C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
 
-	ok := C.fes_new(handle, C.fes_enum_tide_type(tideType), C.fes_enum_access(mode), cpath)
+	ok := C.fes_new(&handle, C.fes_enum_tide_type(tideType), C.fes_enum_access(mode), cpath)
 	if ok == 0 {
-		return &Fes{cpath, handle}, nil
+		return &Fes{&handle}, nil
 	} else if ok == 1 {
 		return nil, fmt.Errorf("Could not create a new FES Session")
 	}
